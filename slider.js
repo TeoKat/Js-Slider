7d7c13d7ab2c56b7e8abb5744dd6c76da81a8d40
TeoKat <52461616+TeoKat@users.noreply.github.com>
2019-09-21 18:22:57 +0300

    Add files via upload

---
+++ slider.js
@@ -0,0 +1,376 @@
+  let pictures = [
+    {img_url: 'https://thumbs.dreamstime.com/z/european-capitals-word-cloud-white-background-european-capitals-word-cloud-139899557.jpg',
+    duration:3000,
+    title: {
+        text: 'EUROPEAN CITIES',
+        color: 'yellow',
+        bgcolor: 'blue',
+        halign: '200px',
+        valign: '20px',
+        fontsize: '1.5em'
+    },
+    summary: {
+        text: 'These are photos from \n few metropolitan European cities  ',
+        color: 'yellow',
+        bgcolor: 'blue',
+        halign: '60px',
+        valign: '-70px',
+        fontsize: '0.8em',
+        padding: '12px'
+    },
+    entry: {
+        fx: 'toleft',
+        duration: '2s'
+    }
+    },
+    {img_url: 'https://thumbs.dreamstime.com/z/parthenon-temple-acropolis-athens-greece-parthenon-temple-acropolis-athens-greece-colorful-103810679.jpg',
+    duration: 3200,
+    title: {
+        text: 'ATHENS',
+        color: 'white',
+        bgcolor: 'blue',
+        halign: '30px',
+        valign: '30px',
+        fontsize: '1.5em'
+    },
+    summary: {
+        text: 'Athens is the capital and\n largest city of Greece.',
+        color: 'white',
+        bgcolor: 'blue',
+        halign: '50px',
+        valign: '-50px',
+        fontsize: '0.8em',
+        padding: '11px'
+    },
+    entry: {
+        fx: 'toleft',
+        duration: '2s'
+    }
+    },
+    {img_url: 'https://thumbs.dreamstime.com/z/paris-16354495.jpg',
+    duration: 3100,
+    title: {
+        text: 'PARIS',
+        color: 'red',
+        bgcolor: 'blue',
+        halign: '25px',
+        valign: '25px',
+        fontsize: '1.5em'
+    },
+    summary: {
+        text: 'Paris is the capital and\n most populous city of France',
+        color: 'red',
+        bgcolor: 'blue',
+        halign: '140px',
+        valign: '-90px',
+        fontsize: '0.8em',
+        padding: '10px'
+    },
+    entry: {
+        fx: 'fadein',
+        duration: '2s'
+    }
+  },
+  {img_url: 'https://thumbs.dreamstime.com/z/london-winter-17892668.jpg',
+    duration: 4000,
+    title: {
+        text: 'LONDON',
+        color: 'red',
+        bgcolor: 'white',
+        halign: '29px',
+        valign: '29px',
+        fontsize: '1.5em'
+    },
+    summary: {
+        text: 'London is the capital and\n largest city of England and\n the United Kingdom, with\n the largest municipal population\n in the European Union.',
+        color: '#fff',
+        bgcolor: '#000',
+        halign: '15px',
+        valign: '-110px',
+        fontsize: '0.8em',
+        padding: '12px'
+    },
+    entry: {
+        fx: 'fadein',
+        duration: '2s'
+    }
+    },
+    {img_url: 'https://thumbs.dreamstime.com/z/lisbon-portugal-20556337.jpg',
+    duration: 4000,
+    title: {
+        text: 'LISBON',
+        color: 'green',
+        bgcolor: 'red',
+        halign: '45px',
+        valign: '45px',
+        fontsize: '1.5em'
+    },
+    summary: {
+        text: 'Lisbon is the capital and\n the largest city of Portugal.',
+        color: 'green',
+        bgcolor: 'red',
+        halign: '120px',
+        valign: '-77px',
+        fontsize: '0.8em',
+        padding: '9px'
+    },
+    entry: {
+        fx: 'toleft',
+        duration: '2s'
+    }
+    },
+    {img_url: 'https://thumbs.dreamstime.com/z/madrid-spain-aerial-view-130069824.jpg',
+    duration: 3200,
+    title: {
+        text: 'MADRID',
+        color: 'yellow',
+        bgcolor: 'red',
+        halign: '150px',
+        valign: '10px',
+        fontsize: '1.5em'
+    },
+    summary: {
+        text: 'Madrid is the capital and\n most populous city of Spain.',
+        color: 'yellow',
+        bgcolor: 'red',
+        halign: '150px',
+        valign: '-60px',
+        fontsize: '0.8em',
+        padding: '13px'
+    },
+    entry: {
+        fx: 'fadein',
+        duration: '2s'
+    }
+},
+{img_url: 'https://thumbs.dreamstime.com/z/berlin-brandenburg-gate-germany-44697739.jpg',
+duration: 2700,
+title: {
+    text: 'BERLIN',
+    color: 'yellow',
+    bgcolor: 'black',
+    halign: '35px',
+    valign: '30px',
+    fontsize: '1.5em'
+},
+summary: {
+    text: 'Berlin  is the capital and\n largest city of Germany by both\n area and population',
+    color: 'yellow',
+    bgcolor: 'black',
+    halign: '20px',
+    valign: '-70px',
+    fontsize: '0.8em',
+    padding: '11px'
+},
+entry: {
+    fx: 'fadein',
+    duration: '3s'
+}
+},
+{img_url: 'https://thumbs.dreamstime.com/z/rome-downtown-13874709.jpg',
+duration: 2700,
+title: {
+    text: 'ROME',
+    color: 'red',
+    bgcolor: 'green',
+    halign: '40px',
+    valign: '20px',
+    fontsize: '1.5em'
+},
+summary: {
+    text: 'Rome is the capital city and\n a special comune of Italy.',
+    color: 'red',
+    bgcolor: 'green',
+    halign: '200px',
+    valign: '-120px',
+    fontsize: '0.8em',
+    padding: '12px'
+},
+entry: {
+    fx: 'fadein',
+    duration: '3s'
+}
+},
+{img_url: 'https://thumbs.dreamstime.com/z/amsterdam-sunset-bird-view-netherlands-capital-cityscape-skyline-nicholas-basilica-winter-time-85971007.jpg',
+duration: 3200,
+title: {
+    text: 'AMSTERDAM',
+    color: 'orange',
+    bgcolor: 'blue',
+    halign: '20px',
+    valign: '25px',
+    fontsize: '1.5em'
+},
+summary: {
+    text: 'Amsterdam is the capital and\n most populous city of the Netherlands.',
+    color:'blue',
+    bgcolor: 'orange',
+    halign: '20px',
+    valign: '-60px',
+    fontsize: '0.8em',
+    padding: '11px'
+},
+entry: {
+    fx: 'toleft',
+    duration: '3s'
+}
+},
+{img_url: 'https://thumbs.dreamstime.com/z/stockholm-sweden-panorama-photo-55573469.jpg',
+duration: 3200,
+title: {
+    text: 'STOCKHOLM',
+    color: 'yellow',
+    bgcolor: 'blue',
+    halign: '10px',
+    valign: '15px',
+    fontsize: '1.5em'
+},
+summary: {
+    text: 'Stockholm is the capital of\n Sweden and the most populous \nurban area in the Nordic countries.',
+    color: 'yellow',
+    bgcolor: 'blue',
+    halign: '220px',
+    valign: '-90px',
+    fontsize: '0.8em',
+    padding: '13px'
+},
+entry: {
+    fx: 'toleft',
+    duration: '2s'
+}
+}
+  ];
+
+let container = document.createElement('div');
+container.id = "container";
+container.className = 'slider';
+document.body.appendChild(container);
+
+
+
+function createDom(elementName,configurationObject) {
+    for(i = 0 ; i<  configurationObject.length; i++){
+        let box = document.createElement('div');
+        box.className = "slide ";
+        let imag = document.createElement('img');
+        imag.src = configurationObject[i].img_url;
+        let heading = document.createElement('div');
+        heading.innerText = configurationObject[i].title.text;
+        let sum = document.createElement('div');
+        sum.innerText = configurationObject[i].summary.text;
+
+        elementName.appendChild(box);
+        box.appendChild(heading);
+        box.appendChild(imag);
+        box.appendChild(sum);
+
+        // C S S     
+        
+        // body
+
+        document.body.style.textAlign = 'center';
+        document.body.style.boxSizing = 'border-box';
+        document.body.style.backgroundColor = 'orange';
+
+        //container [ .slider]        
+
+        elementName.style.position = "relative";
+        elementName.style.width = "600px";
+        elementName.style.height = "400px";
+        elementName.style.margin = "auto";
+        elementName.style.overflow = "hidden";
+        elementName.style.boxSizing = "border-box";
+        elementName.style.padding = "0px";        
+                       
+        //B o x[.slide]
+
+        box.style.position="absolute";
+        box.style.width="600px";
+        box.style.height="400px";
+        box.style.textAlign="center";
+        box.style.background="red";
+        box.style.color="white";
+        box.style.border="10px solid black";
+        box.style.padding="0px";
+        box.style.boxSizing="border-box";       
+        box.style.transition="opacity " + configurationObject[i].entry.duration + " , transform "+ configurationObject[i].entry.duration;   
+        
+        // imag 
+
+       imag.style.width = "100%";
+       imag.style.height = "380px";
+       imag.style.boxSizing = "border-box";       
+
+       // heading
+
+       heading.style.position = "absolute";
+       heading.style.zIndex = "100";
+       heading.style.color = configurationObject[i].title.color;
+       heading.style.backgroundColor = configurationObject[i].title.bgcolor;
+       heading.style.fontsize = configurationObject[i].title.fontsize;
+       heading.style.marginLeft = configurationObject[i].title.halign;
+       heading.style.marginTop= configurationObject[i].title.valign;
+       heading.style.fontWeight= "bold";       
+       heading.style.borderRadius= "10%";
+       
+       // summary
+
+       sum.style.position = "absolute";
+       sum.style.zIndex = "100";
+       sum.style.color = configurationObject[i].summary.color;
+       sum.style.backgroundColor = configurationObject[i].summary.bgcolor;
+       sum.style.fontsize = configurationObject[i].summary.fontsize;
+       sum.style.padding = configurationObject[i].summary.padding;
+       sum.style.marginLeft = configurationObject[i].summary.halign;
+       sum.style.marginTop= configurationObject[i].summary.valign;
+       sum.style.borderRadius= "40%";       
+    }
+};
+
+
+
+let slideShow = function(elementName,configurationObject) {
+
+   let images = [];
+    let curImage = 0;
+    for (i = 0; i < elementName.childElementCount; i++) {
+     images.push(elementName.children[i]);    
+    }
+
+    let nextSlide = function(elementId) {
+        if(configurationObject[curImage].entry.fx == "fadein") {
+     images[curImage].style.transform = "translate(0,0)";
+     images[curImage].style.opacity = 1;
+    } else if(configurationObject[curImage].entry.fx == "toleft"){    
+        for (let i = 0; i < images.length; i++) { 
+                images[i].style.transform = "translate(-100%,0)"; 
+                images[i].style.opacity = "0";           
+        };
+        images[curImage].style.transform = "translate(0,0)";
+        images[curImage].style.opacity = 1;
+    };    
+      curImage++;    
+      if(curImage >= images.length ) curImage=0; 
+      if(configurationObject[curImage].entry.fx == "fadein") {
+        images[curImage].style.transform = "translate(0,0)";
+        images[curImage].style.opacity =0;
+      };
+      if(configurationObject[curImage].entry.fx == "toleft") {
+        images[curImage].style.transform = "translate(100%,0)";
+        images[curImage].style.opacity =0;
+      };
+      window.setTimeout(nextSlide.bind(elementId), configurationObject[curImage].duration);
+    };    
+    nextSlide(elementName);   
+  };
+
+function Slider(elementName,configurationObject) {
+    createDom(elementName,configurationObject);
+    slideShow(elementName,configurationObject);
+};
+ 
+Slider(container,pictures);
+
+
+
+
